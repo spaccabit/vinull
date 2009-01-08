99bfe4a49836c211469c8412ac12d8e89e4dda1a
michael.neel <michael.neel@f63322af-f64c-0410-bcff-ab9438e7f5fe>
2009-01-08 03:45:58 +0000

    epoch

---
+++ Presentations/DI-WayOfTheNinja/DI-WayOfTheNinja/Turtles/Foot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DI_WayOfTheNinja.Combat;
+
+namespace DI_WayOfTheNinja.Turtles {
+    
+    public class Foot : Warrior {
+        public Foot() {
+            Name = "Foot Soldier";
+            MainWeapon = new Sword();
+        }
+    }
+}
