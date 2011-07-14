d4af4ee2c705a8c00d5404042e067b4c5efcba8f
michael.neel@gmail.com <michael.neel@gmail.com@f63322af-f64c-0410-bcff-ab9438e7f5fe>
2011-07-14 01:28:10 +0000

    epoch

---
+++ Examples/InvadersFromSpace/InvadersFromSpace/InvadersFromSpace/Armada.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using InvadersFromSpace.Objects;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace InvadersFromSpace {
+    public class Armada {
+
+        public Invader[] Invaders;
+        static Color[] InvaderColors = new Color[] { Color.Purple, Color.Blue, Color.Red, Color.Orange, Color.Yellow, Color.Silver };
+        
+        Int32 ArmadaFrame;
+        double ArmadaMoveTimer;
+        double ArmadaMoveRate = 50;
+        Int32 ArmadaSpeed = 5;
+        Int32 ArmadaDirection = 1;
+        Rectangle ArmadaLocation;
+        Rectangle Field;
+
+        Int32 startx;
+        Int32 starty;
+        public Boolean Landed;
+
+        public Armada(Int32 rows, Int32 cols, Int32 x, Int32 y, Rectangle field) {
+
+            Invaders = new Invader[rows * cols];
+            Field = field;
+            startx = x;
+
+            for (int r = 0; r < rows; r++) {
+                for (int c = 0; c < cols; c++) {
+                    Invaders[c + r * cols] = Invader.CreateInvader(SpriteSheet.Invader1,
+                        field.X + c * SpriteSheet.Invader1[0].Height + c * 10,
+                        field.Y + r * SpriteSheet.Invader1[0].Width + r * 10,
+                        InvaderColors[r]);
+                }
+            }
+            ArmadaLocation = new Rectangle(Invaders[0].Location.X, Invaders[0].Location.Y,
+                Invaders[Invaders.Length - 1].Location.X + Invaders[Invaders.Length - 1].Location.Width - Invaders[0].Location.X,
+                Invaders[Invaders.Length - 1].Location.Y + Invaders[Invaders.Length - 1].Location.Height - Invaders[0].Location.Y);
+            ArmadaFrame = 0;
+            ArmadaMoveTimer = 0;
+            Landed = false;
+        }
+
+        public void Update(GameTime gameTime) {
+
+            ArmadaMoveTimer += gameTime.ElapsedGameTime.TotalMilliseconds;
+            if (ArmadaMoveTimer >= ArmadaMoveRate) {
+                ArmadaMoveTimer = 0;
+                ArmadaFrame = ArmadaFrame == 0 ? 1 : 0;
+                ArmadaLocation.X += ArmadaSpeed * ArmadaDirection;
+                if (!Field.Contains(ArmadaLocation)) {
+                    ArmadaLocation.X -= ArmadaSpeed * ArmadaDirection;
+                    ArmadaDirection *= -1;
+                    ArmadaLocation.X += ArmadaSpeed * ArmadaDirection;
+
+                    ArmadaLocation.Y += 20;
+                    if (!Field.Contains(ArmadaLocation)) {
+                        for (int i = 0; i < Invaders.Length; i++) {
+                            Invaders[i].Active = false;
+                            Landed = true;
+                        }
+                    }
+                    else {
+                        for (int i = 0; i < Invaders.Length; i++) {
+                            Invaders[i].Location.Y += 20;
+                        }
+                    }
+                }
+                else {
+                    for (int i = 0; i < Invaders.Length; i++) {
+                        Invaders[i].Location.X += ArmadaSpeed * ArmadaDirection;
+                    }
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch) {
+            for (int i = 0; i < Invaders.Length; i++)
+                Invader.Draw(spriteBatch, Invaders[i], ArmadaFrame);
+        }
+
+    }
+}
---
+++ Examples/InvadersFromSpace/InvadersFromSpace/InvadersFromSpace/SpriteSheet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace InvadersFromSpace {
+    public struct SpriteSheet {
+
+        public static Texture2D Texture;
+
+        public static Rectangle PlayerCannon = new Rectangle(5, 0, 22, 32);
+        public static Rectangle Bullet = new Rectangle(51, 0, 5, 16);
+        public static Rectangle Solid = new Rectangle(73, 1, 34, 34);
+        public static Rectangle[] Invader1 = new Rectangle[] {
+            new Rectangle(113, 10, 26, 22),
+            new Rectangle(149, 10, 26, 22)
+        };
+
+    }
+}
